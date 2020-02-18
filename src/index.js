515a867f2db987d0c341837cff549091841e4019
Biodun <biodun9@gmail.com>
2020-02-18 14:06:41 +0100

    added linting files and text for testing app deployment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = (props) => {
+    const styling={color:'coral', textAlign:'center'}
+    return(
+        <div style={styling}>
+        <h1 > Testing the Deployment of this New React App to {props.name}</h1>
+        </div>
+    )
+}
 
+
+ReactDOM.render(<App name="Heroku"/>,document.getElementById('root'))
