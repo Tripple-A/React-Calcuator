2e933b33823ab4f631db1766fa93549657ca493a
Tripple-A <biodun9@gmail.com>
2020-02-19 21:33:51 +0100

    added changes to calculate.js and operate.js to include more edge cases

--- src/logic/Calculate.js
+++ src/logic/Calculate.js
@@ -1,18 +1,47 @@
 import Operate from './Operate';
 
-const Calculate = (data, name) => {
-  let { total, next } = data;
-  const { operation } = data;
-  if (name === '+/-') {
+const Calculate = (data, buttonName) => {
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const ops = ['X', '-', '+', '÷'];
+  let { total, next, operation } = data;
+
+  if (buttonName === '+/-') {
     total *= (-1);
     next *= (-1);
+  } else if (buttonName === '=' || buttonName === '%' || (ops.includes(buttonName) && next && operation)) {
+    const newData = {
+      total: parseFloat(total),
+      next: parseFloat(next),
+      operation,
+    };
+    total = Operate(newData.total, newData.next, operation).toString();
+    next = null;
+    operation = buttonName;
+  } else if (buttonName === 'AC') {
+    total = null;
+    next = null;
+  } else if (buttonName === '.') {
+    if (!next && !total) { total = '0.'; next = '0.'; } else if (!next && operation) {
+      next = '0.';
+    } else if (operation && total && next) {
+      next += buttonName;
+    } else {
+      next += '.';
+      total += '.';
+    }
+  } else if (nums.includes(buttonName)) {
+    if (!next && total) { next = buttonName; } else if (next && total && !operation) {
+      total += buttonName;
+      next += buttonName;
+    } else if (next && operation) {
+      next += buttonName;
+    }
   }
-  const newData = {
-    total: parseFloat(total),
-    next: parseFloat(next),
+  return {
+    total,
+    next,
     operation,
   };
-  Operate(newData.total, newData.next, operation);
 };
 
 
--- src/logic/Operate.js
+++ src/logic/Operate.js
@@ -1,20 +1,17 @@
 const Big = require('big.js');
 
-const Operate = ((num1, num2, operator) => {
-  switch (operator) {
-    case '+':
-      return Big(num1).plus(Big(num2));
-    case '-':
-      return Big(num1).minus(Big(num2));
-    case '×':
-      return Big(num1).mul(Big(num2));
-    case '%':
-      return Big(num1).mod(Big(num2));
-    case '÷':
-      return Big(num1).div(Big(num2));
-    default:
-      return '';
-  }
-})();
+const Operate = (num1, num2, operator) => {
+  if (operator === '+') { return Big(num1).plus(Big(num2)); }
+
+  if (operator === '-') { return Big(num1).minus(Big(num2)); }
+
+  if (operator === '÷') { return num2 === 0 ? 'Infinity' : Big(num1).div(Big(num2)); }
+
+  if (operator === '%') { return Big(num1).div(Big(100)); }
+
+  if (operator === 'X') { return Big(num1).mul(Big(num2)); }
+  return null;
+};
+
 
 export default Operate;
